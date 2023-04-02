5c48c312643ffc3eb373b03a60a5b8c3030f8eb2
Camilo A. Huertas C <124012692+camandhuercue@users.noreply.github.com>
2023-04-01 23:17:08 -0500

    Create setup.py

---
+++ Entrega_3/Worker/Queue/setup.py
@@ -0,0 +1,21 @@
+'''
+Setup of the python package for the API 
+'''
+from setuptools import setup
+
+setup(
+    name='queue_api',
+    packages=['queue_api'],
+    include_package_data=True,
+    install_requires=[
+        'celery',
+        'redis',
+        'SQLAlchemy',
+        'marshmallow',
+        'psycopg2-binary',
+        'zipfile36',
+        'py7zr',
+        'bz2file',
+        'secure-smtplib'
+    ],
+)
