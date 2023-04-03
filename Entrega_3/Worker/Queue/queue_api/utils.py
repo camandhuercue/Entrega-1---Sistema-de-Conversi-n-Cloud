81c2146a84ddc5873f7f71f2a59198ebc8f152d9
Camilo A. Huertas C <124012692+camandhuercue@users.noreply.github.com>
2023-04-02 21:45:51 -0500

    Update utils.py

--- Entrega_3/Worker/Queue/queue_api/utils.py
+++ Entrega_3/Worker/Queue/queue_api/utils.py
@@ -1,20 +1,7 @@
-from queue_api import Base
-import sqlalchemy
-import marshmallow
+from queue_api.models import Tasks_TB, tasks_schema
+from queue_api import session, engine
 
-class Tasks_TB(Base):
-    __tablename__ = 'tasks'
-    __table_args__ = dict(schema="compress_schema")
-    id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.Identity(start=1, cycle=True), nullable = False, primary_key = True)
-    email = sqlalchemy.Column(sqlalchemy.String(100))
-    status = sqlalchemy.Column(sqlalchemy.String(50))
-    timestamp = sqlalchemy.Column(sqlalchemy.DateTime())
-    filename = sqlalchemy.Column(sqlalchemy.String(500))
-    path = sqlalchemy.Column(sqlalchemy.String(1000))
-    format = sqlalchemy.Column(sqlalchemy.String(50))
 
-class TasksSchema(marshmallow.Schema):
-    class Meta:
-        fields = ("id", "path", "filename", "email", "format")
-
-tasks_schema = TasksSchema(many = True)
+def get_tasks():
+    tasks = session.query(Tasks_TB).filter_by(status='uploaded')
+    return tasks_schema.dump(tasks)
