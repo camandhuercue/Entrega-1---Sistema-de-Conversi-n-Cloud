219b6cdfa47365653f63b26d82d27bd1bbd3e926
Camilo A. Huertas C <124012692+camandhuercue@users.noreply.github.com>
2023-05-07 16:08:30 -0500

    Delete routes.py

--- Entrega_4/Backend_base/BackEnd/compress_api/routes.py
+++
@@ -1,8 +0,0 @@
-from compress_api import api
-from compress_api.views import SignUp, Login, Tasks, Tasks_id, get_file
-
-api.add_resource(SignUp, '/api/auth/signup')
-api.add_resource(Login, '/api/auth/login')
-api.add_resource(Tasks, '/api/tasks')
-api.add_resource(Tasks_id, '/api/tasks/<path:id_task>')
-api.add_resource(get_file, '/api/files/<path:file_name>')
